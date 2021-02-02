91975db0cf2feb90bc2596c002873650524e0ac5
leo <leo_xhj@hotmail.com>
2021-02-02 18:13:38 +0800

    add role: lvmd service

--- roles/volumegroup/files/init_disk.sh
+++ roles/volumegroup/files/init_disk.sh
@@ -12,11 +12,5 @@ do
 done
 
 vgcreate VolGroup01 /dev/sd[b,c]1 -v
-lvcreate -l 100%VG -i${num} -I 131072 VolGroup01 -n LogVol00
-lvdisplay -m
 
-#mkfs.ext4 /dev/mapper/VolGroup01-LogVol00
-mke2fs -O 64bit,has_journal,extents,huge_file,flex_bg,uninit_bg,dir_nlink,extra_isize /dev/mapper/VolGroup01-LogVol00
-
-grep -vq "VolGroup01-LogVol00" /etc/fstab && echo "/dev/mapper/VolGroup01-LogVol00      /var/data  ext4    defaults        0 0" >> /etc/fstab
 
